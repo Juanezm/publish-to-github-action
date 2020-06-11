3c10e50694229705f8cd9c5698b506707111d8bd
Juan Emilio Zurita <juanezm@ieee.org>
2020-06-11 12:05:25 +0100

    Update entrypoint.sh
    Adding COMMIT_MSG for custom commit messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ fi
 # initialize git
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git config http.sslVerify false
-git config user.name "Automated Publisher"
-git config user.email "actions@users.noreply.github.com"
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git remote add publisher "${remote_repo}"
 git show-ref # useful for debugging
 git branch --verbose
@@ -21,7 +21,6 @@ git lfs install
 # publish any new files
 git checkout master
 git add -A
-timestamp=$(date -u)
-git commit -m "Automated publish: ${timestamp} ${GITHUB_SHA}" || exit 0
+git commit -m "${COMMIT_MSG}" || exit 0
 git pull --rebase publisher master
 git push publisher master
